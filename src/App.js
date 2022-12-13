d73f9c041f8b56b4d64067d926fc09ed473d3567
4bhish <108976136+4bhish@users.noreply.github.com>
2022-12-13 21:26:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+const [userText,setUserText]=useState("");
+const [btnStatus,setBtnStatus] = useState(false);
+const [outputText,setOutputText] = useState("")
+const [errorMsg,setErrorMsg] = useState("")
+const fetchUrl =( `https://api.funtranslations.com/translate/minion.json?text=${userText}`);
+
+  function handleChange(event){
+    setUserText(event.target.value);
+  }
+
+  useEffect(()=>{
+    if(btnStatus){
+      fetch(fetchUrl)
+    .then(res =>  {
+      if(!res.ok){
+      throw Error("Error: An error occured because busy servers try after some time.")
+    } 
+    return res.json()})
+    .then(data => setOutputText(data.contents.translated))
+    .catch(Error => {
+      setErrorMsg(Error.message)
+    })
+    setBtnStatus(false)
+    }
+  },[btnStatus])
+
+  function handleClick(){
+    setBtnStatus(true)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <h1 className="heading">Speak Banana</h1>
+        <p className="desc">Welcome to Banana Speak</p>
+        <textarea onChange={handleChange} type="text"/>
+        <button onClick={handleClick}>Translate</button>
+        <p>Your Translation will appear here :</p>
+        <div className="outputText">{outputText}</div>
+        <h4>{errorMsg}</h4>
+      </div>
     </div>
   );
 }
